43c173402a78d78a5065061ecac6891f5111a4c6
Lukas Matt <lukas@zauberstuhl.de>
2016-09-17 13:47:49 +0200

    Removed db config and add license

---
+++ README.md
@@ -0,0 +1,2 @@
+Diaspberry Wizard
+=================
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
+# Diaspberry Wizard
+#
+# Copyright (C) 2016 Lukas Matt <lukas@zauberstuhl.de>,
+# Matthias Neutzner <matthias.neutzner@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def show
+  end
 end
